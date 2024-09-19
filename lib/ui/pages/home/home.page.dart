08990b52df763eb4dec0ef075dc48e7d29e01523
Marcelo Freitas <marcelofreitasx@gmail.com>
2024-09-19 11:20:16 -0300

    feat: create home page ui

--- lib/ui/pages/home/home.page.dart
+++ lib/ui/pages/home/home.page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../components/components.dart';
+import '../../styles/styles.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: SvgPicture.asset(AppAssets.logo),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CompanyButton(
+              title: "Jaguar Unit",
+              onPressed: () {
+                //TODO: Implement onPressed function
+              },
+            ),
+            const SizedBox(height: 40),
+            CompanyButton(
+              title: "Tobias Unit",
+              onPressed: () {
+                //TODO: Implement onPressed function
+              },
+            ),
+            const SizedBox(height: 40),
+            CompanyButton(
+              title: "Apex Unit",
+              onPressed: () {
+                //TODO: Implement onPressed function
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
